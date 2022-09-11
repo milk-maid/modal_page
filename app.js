3a194cf52f5715db61970cbbc7e254f6d2b98b64
Bello Roqeeb <roqbell@gmail.com>
2022-09-11 09:58:29 +0100

    okay

---
+++ app.js
@@ -0,0 +1,9 @@
+let popup = document.querySelector('#popup');
+
+function openPopup() {
+    popup.classList.add('open_popup');
+}
+
+function closePopup() {
+    popup.classList.remove('open_popup');
+}
